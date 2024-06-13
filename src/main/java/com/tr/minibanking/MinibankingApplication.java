9eed37429effd4a007d983f662481265abd7b597
Bahar KALKAN <bahar.eren@assistt.com.tr>
2024-06-14 00:25:07 +0300

    Initial commit: Set up Spring Boot project structure and dependencies

---
+++ src/main/java/com/tr/minibanking/MinibankingApplication.java
@@ -0,0 +1,13 @@
+package com.tr.minibanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibankingApplication.class, args);
+	}
+
+}
