5545302bd47dde2e3db1b75e880fedf05663db67
Bahar KALKAN <bahar.eren@assistt.com.tr>
2024-06-18 02:35:49 +0300

    Api response base body added

---
+++ src/main/java/com/tr/minibanking/model/ApiResponse.java
@@ -0,0 +1,40 @@
+package com.tr.minibanking.model;
+
+import com.tr.minibanking.enums.Message;
+import org.springframework.http.HttpStatus;
+
+public class ApiResponse<T> {
+  private HttpStatus status;
+  private String message;
+  private T data;
+
+  public ApiResponse(HttpStatus status, Message message, T data) {
+    this.status = status;
+    this.message = message.getMessage();
+    this.data = data;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(HttpStatus status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+}
