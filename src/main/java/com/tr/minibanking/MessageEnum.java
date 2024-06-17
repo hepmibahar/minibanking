6c02862df4b23d060886197c107d953548326c37
Bahar KALKAN <bahar.eren@assistt.com.tr>
2024-06-17 22:29:20 +0300

    Message added

--- src/main/java/com/tr/minibanking/MessageEnum.java
+++ src/main/java/com/tr/minibanking/MessageEnum.java
@@ -2,6 +2,7 @@ package com.tr.minibanking;
 
 public enum MessageEnum {
   USER_ALREADY("User is already registered!"),
+  USER_EMAIL_ALREADY("User email is already registered!"),
   TRANSACTION_SUCCESSFUL("Transaction successful.");
 
   private final String message;
