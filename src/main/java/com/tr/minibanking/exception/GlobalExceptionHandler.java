66a08065a5018012ec27ef22af57e84f28940e18
Bahar KALKAN <bahar.eren@assistt.com.tr>
2024-06-18 09:40:15 +0300

    Global exception handler eklendi.

---
+++ src/main/java/com/tr/minibanking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.tr.minibanking.exception;
+
+import com.tr.minibanking.enums.Message;
+import com.tr.minibanking.model.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+
+    ApiResponse<Map<String, String>> response = new ApiResponse<>(HttpStatus.BAD_REQUEST, Message.VALIDATION_FAILED, errors);
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+}
