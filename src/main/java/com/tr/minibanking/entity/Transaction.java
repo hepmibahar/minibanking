3a4d907c509511478f8f5b4329cf10c0f642d91c
Bahar KALKAN <bahar.eren@assistt.com.tr>
2024-06-21 17:23:04 +0300

    Implement transaction service, repository, and controller with status handling using ransactionStatus enum

--- src/main/java/com/tr/minibanking/entity/Transaction.java
+++ src/main/java/com/tr/minibanking/entity/Transaction.java
@@ -1,12 +1,19 @@
 package com.tr.minibanking.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
-
-import com.tr.minibanking.enums.TransactionStatus;
 
 @Data
 @Entity
